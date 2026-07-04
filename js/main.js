1f9b90d251f781e4253c3293ddf582b28977658a
AkTSMART <interioarchindia@gmail.com>
2026-07-04 14:16:12 +0530

    Implement navbar shadow on scroll
    Add scroll event listener to toggle navbar shadow

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
-console.log("AKTSmart Technologies Website Loaded");
+window.addEventListener("scroll",function(){
+
+const nav=document.querySelector(".navbar");
+
+if(window.scrollY>80){
+
+nav.classList.add("shadow-lg");
+
+}else{
+
+nav.classList.remove("shadow-lg");
+
+}
+
+});
